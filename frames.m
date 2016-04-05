25edaa01e896b5799215e338937e67a926efb94a
TobiNakamura <tobishires@gmail.com>
2016-04-04 17:17:24 -0700

    auto path generation
    begin

--- frames.m
+++ frames.m
@@ -3,26 +3,37 @@ path = [0 0 1; -3 -3 1];
 WIDTH = 300;
 HEIGHT = 200;
 DEFAULT_ZOOM = 1; %sets the difference in real value
-
-angle = atan((path(2,2)-path(1,2))/(path(2,1)-path(1,1)))
-realWidth = DEFAULT_ZOOM
-imgHeight = DEFAULT_ZOOM*HEIGHT/WIDTH
-initial = corners(path(1,1:2))
-final = corners(path(2,1:2))
-distance = path(2,1:2) - path(1,1:2)
-n=ceil(abs(distance)/realWidth-1)*2+2
-frames = ones(n, 4);
-dir = distance/abs(distance)
-for k=2:n
-    frames(k) = corners(path(1,1:2)+dir*realWidth*k) %main frame
-    frames(k+1) = []
-    frames(k+2) = []
-    k=k+3
+numFrames = 5;
+full_path = zeros(numFrames, 3);
+full_path(1, :) = path(1, :);
+full_path(end, :) = path(2, :);
+vect = (path(2,:) - path(1,:))/(numFrames-1);
+for k=2:(numFrames-1)
+    full_path(k,:) = full_path(k-1,:)+vect;
 end
 
+    function c = corners(center)
+        matrix = [center(1)*ones(4,1)+[-1 1 -1 1]'*realWidth/2 center(2)*ones(4,1)+[1 1 -1 -1]'*imgHeight/2];
+        c = [matrix(1, :) matrix(2, :) matrix(3, :) matrix(4, :)];
+    end
 
-function c = corners(center)
-    c = [center(1)*ones(4,1)+[-1 1 -1 1]'*realWidth/2 center(2)*ones(4,1)+[1 1 -1 -1]'*imgHeight/2];
-end
+% angle = atan((path(2,2)-path(1,2))/(path(2,1)-path(1,1)))
+% realWidth = DEFAULT_ZOOM
+% imgHeight = DEFAULT_ZOOM*HEIGHT/WIDTH
+% distance = path(2,1:2) - path(1,1:2)
+% n=ceil(norm(distance)/realWidth-1)*2+4
+% frames = ones(n, 8);
+% corners(path(1,1:2))
+% frames(1, :) = corners(path(1,1:2))
+% frames(end, :) = corners(path(2,1:2))
+% dir = distance/abs(distance)
+% for k=2:n-1
+%     frames(k, :) = corners(path(1,1:2)+dir*realWidth*k)
+%     frames(k+1, :) = [frames(k-2)]
+%     frames(k+2, :) = []
+%     k=k+3
+% end
 
+
+    
 end
